d9f31aef2199a8a812ba1b7e4fe3750a0da626b8
vincent50117 <vincent50117@gmail.com>
2018-12-10 08:20:39 +0800

    fighting animation fix
    update 4 of the animation

---
+++ Assets/Scripts/CameraController1.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController1 : MonoBehaviour {
+
+    public GameObject player;
+    private Vector3 offset;
+    public float rightmost, leftmost;
+
+    // Use this for initialization
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 postPostion = transform.position;
+        transform.position = player.transform.position + offset;
+        if (transform.position.x > rightmost || transform.position.x < leftmost) transform.position = postPostion + new Vector3(0, transform.position.y - postPostion.y, 0);
+        
+    }
+
+}
